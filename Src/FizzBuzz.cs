d8edb7287a1ff381a4c6b77ab536d523e17e736b
Gonzalo Albito Méndez <gmendez@tecnosylva.com>
2025-02-12 11:17:48 +0100

    chore: limits

--- Src/FizzBuzz.cs
+++ Src/FizzBuzz.cs
@@ -4,6 +4,14 @@ public class FizzBuzz
 {
     public static string Convert(int number)
     {
+        if(number<=0)
+        {
+            throw new ArgumentOutOfRangeException("Number should be greater than zero");
+        }
+        if(number>=101)
+        {
+            throw new ArgumentOutOfRangeException("Number should be lower than 101");
+        }
         return number.ToString();
     }
 }
--- Tests/FizzBuzzTest.cs
+++ Tests/FizzBuzzTest.cs
@@ -6,6 +6,61 @@ namespace FizzBuzzKata.Tests;
 public class FizzBuzzTests
 {
     //private static readonly int[] DIV3 = [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99];
+
+
+    [Fact]
+    public void EnsureNumberIsNotZero()
+    {
+        int number = 0;
+        Assert.Throws<ArgumentOutOfRangeException>(()=>{
+                FizzBuzz.Convert(number);
+            });
+    }
+
+    [Fact]
+    public void EnsureNumberIsNotMinusOne()
+    {
+        int number = -1;
+        Assert.Throws<ArgumentOutOfRangeException>(()=>{
+            FizzBuzz.Convert(number);
+        });
+    }
+
+    [Fact]
+    public void EnsureNumberIsNotMinusTwo()
+    {
+        int number = -2;
+        Assert.Throws<ArgumentOutOfRangeException>(()=>{
+            FizzBuzz.Convert(number);
+        });
+    }
+
+    [Fact]
+    public void EnsureNumberIsNot101()
+    {
+        int number = 101;
+        Assert.Throws<ArgumentOutOfRangeException>(()=>{
+            FizzBuzz.Convert(number);
+        });
+    }
+
+    [Fact]
+    public void EnsureNumberIsNot102()
+    {
+        int number = 102;
+        Assert.Throws<ArgumentOutOfRangeException>(()=>{
+            FizzBuzz.Convert(number);
+        });
+    }
+
+    [Fact]
+    public void EnsureNumberIsNot103()
+    {
+        int number = 103;
+        Assert.Throws<ArgumentOutOfRangeException>(()=>{
+            FizzBuzz.Convert(number);
+        });
+    }
     
     [Fact]
     public void ReturnNumberOneAsText()
